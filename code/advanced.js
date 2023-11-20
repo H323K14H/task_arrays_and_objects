0de7703cf563f80fc9398a5a5eea3cfc64ddd8de
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-20 21:22:04 +0000

    Question 4 complete using Math.max and Math.min to compare the population size of the scotland variable to the other elements in the unitedKingdom array

--- code/advanced.js
+++ code/advanced.js
@@ -32,7 +32,7 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = unitedKingdom.find(country => country.name === "England");
+const england = unitedKingdom.find(country => country.name === "England");
 england.touristAttractions = ["London eye", "Morleys", "London Gallery"];
 
 console.log(england);
@@ -41,7 +41,7 @@ console.log(england);
  * Q2. Change the capital of Wales to "Cardiff"
  */
 
-wales = unitedKingdom.find(country => country.name === "Wales");
+const wales = unitedKingdom.find(country => country.name === "Wales");
 wales.capital = "Cardiff";
 
 console.log(wales);
@@ -52,7 +52,7 @@ console.log(wales);
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIreland = unitedKingdom.find(country => country.name === "Northern Ireland");
+const northernIreland = unitedKingdom.find(country => country.name === "Northern Ireland");
 northernIrelandKeys = Object.keys(northernIreland);
 
 console.log(northernIrelandKeys);
@@ -62,3 +62,16 @@ console.log(northernIrelandKeys);
 /**
  * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
  */
+
+const scotland = unitedKingdom.find(country => country.name === "Scotland");
+
+const largestPopulation = Math.max(england.population, wales.population, northernIreland.population);
+const smallestPopulation = Math.min(england.population, wales.population, northernIreland.population);
+
+if (scotland.population > largestPopulation) {
+  console.log("Scotland has the largest population.");
+} else if (scotland.population < smallestPopulation) {
+  console.log("Scotland has the smallest population.");
+} else {
+  console.log("Scotland has a population in the middle of the other countries.");
+}
