a471994808dfdefa85b1a8f1fefae51b49d3dd03
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-20 21:16:30 +0000

    Question 3 complete

--- code/advanced.js
+++ code/advanced.js
@@ -52,7 +52,10 @@ console.log(wales);
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
+northernIreland = unitedKingdom.find(country => country.name === "Northern Ireland");
+northernIrelandKeys = Object.keys(northernIreland);
+
+console.log(northernIrelandKeys);
 
 // console.log(northernIrelandKeys);
 
